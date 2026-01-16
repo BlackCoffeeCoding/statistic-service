6a96da36266bee91d14d3f76cac2f659b94541b6
BlackCoffeeCoding <kvsolntsev@gmail.com>
2026-01-16 16:13:36 +0300

    before lab 12 stage fixes

--- src/main/java/org/blackcoffeecoding/statistics/listeners/StatisticsEventListener.java
+++ src/main/java/org/blackcoffeecoding/statistics/listeners/StatisticsEventListener.java
@@ -21,34 +21,28 @@ public class StatisticsEventListener {
 
     private static final Logger log = LoggerFactory.getLogger(StatisticsEventListener.class);
 
-    // Храним статистику: Категория -> Количество
     private final Map<String, Integer> stats = new ConcurrentHashMap<>();
 
     @RabbitListener(bindings = @QueueBinding(
             value = @Queue(name = "statistics-queue", durable = "true"),
             exchange = @Exchange(name = "devices-exchange", type = "topic", durable = "true"),
-            key = "device.created" // Слушаем только создание
+            key = "device.created"
     ))
     public void handleDeviceCreated(DeviceCreatedEvent event,
                                     Channel channel,
                                     @Header(AmqpHeaders.DELIVERY_TAG) long deliveryTag) throws IOException {
         try {
-            // Логика статистики
+
             stats.merge(event.category(), 1, Integer::sum);
 
             log.info("STATS: Учтено новое устройство: {}. Категория: {}. Всего в категории: {}",
                     event.name(), event.category(), stats.get(event.category()));
 
-            // РУЧНОЕ ПОДТВЕРЖДЕНИЕ УСПЕХА
-            // false означает, что мы подтверждаем только это одно сообщение
             channel.basicAck(deliveryTag, false);
 
         } catch (Exception e) {
             log.error("Ошибка при обновлении статистики", e);
 
-            // Если ошибка - отклоняем сообщение.
-            // requeue=false (не возвращать) или true (вернуть) зависит от логики.
-            // Для статистики обычно false, чтобы не зациклить ошибку.
             channel.basicNack(deliveryTag, false, false);
         }
     }
